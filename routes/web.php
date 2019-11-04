51985577863c1bca00da6fa9d39f77bc56164c5d
oscar-mx <oscarmx0912@outlook.com>
2019-11-04 19:55:59 +0800

    代码生成器&话题

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,4 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
 //user info routes...
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
