8b83dc13dd66009fea164453aac8f44bec08d0a9
oscar-mx <oscarmx0912@outlook.com>
2019-11-12 21:16:57 +0800

    topic template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>@yield('title', 'mxbbs') - mxbbs</title>
+    <meta name="description" content="@yield('description', 'mxbbs 社区')" />
 
     <title>@yield('title', 'mxbbs')</title>
 
